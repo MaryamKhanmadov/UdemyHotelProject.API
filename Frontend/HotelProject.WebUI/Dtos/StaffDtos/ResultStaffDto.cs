69d192b801e6d5d70aec7ff75ac024ad743ca928
MaryamKhanmadova <maryamkhanmadov@gmail.com>
2023-11-13 02:51:49 +0400

    add part of index page

---
+++ Frontend/HotelProject.WebUI/Dtos/StaffDtos/ResultStaffDto.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelProject.WebUI.Dtos.StaffDtos
+{
+    public class ResultStaffDto
+    {
+        public int id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeactive { get; set; } = false;
+        public required string Name { get; set; }
+        public required string Title { get; set; }
+        public required string Image { get; set; }
+        public required string SocialMedia1 { get; set; }
+        public required string SocialMedia2 { get; set; }
+        public required string SocialMedia3 { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/TestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelProject.WebUI.Dtos.TestimonialDtos
+{
+    public class ResultTestimonialDto
+    {
+        public int id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeactive { get; set; } = false;
+        public required string Name { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string Image { get; set; }
+    }
+}
