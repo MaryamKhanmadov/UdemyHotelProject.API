70cf559202cb0113f2ef84ca3309c397833cfc9b
MaryamKhanmadova <maryamkhanmadov@gmail.com>
2023-11-26 02:42:23 +0400

    add adminAboutUpdate

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminAboutController.cs
@@ -0,0 +1,60 @@
+﻿using HotelProject.WebUI.Dtos.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Nodes;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5278/api/About");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5278/api/About/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonDataString = await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = JsonObject.Parse(jsonDataString); // jsonDataString, JSON datanızın olduğu dəyişkəndir
+
+                var values = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData["result"].ToString());
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAboutDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5278/api/About/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
